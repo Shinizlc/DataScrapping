81ecfee5fe7d724dd35f0c12af4e1f2ae97851b4
Aleksei <shinizlc@gmail.com>
2021-03-03 15:02:07 +0300

    mvideo third commit

--- Lesson5/mvideo.py
+++ Lesson5/mvideo.py
@@ -14,17 +14,26 @@ driver.get('https://www.mvideo.ru/')
 driver.maximize_window()
 try:
     list_of_data=[]
-    data_to_mongo={}
-    #next_button=driver.find_element_by_css_selector('.next-btn.c-btn.c-btn_scroll-horizontal.c-btn_icon.i-icon-fl-arrow-right')
-    for i in range(0,6):
+   # len_of_data=0
+    for i in range(0,3):
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'.next-btn.c-btn.c-btn_scroll-horizontal.c-btn_icon.i-icon-fl-arrow-right'))).click()  # doesn't resolve the error ElementClickInterceptedException
         sleep(3)
-    sale_hits=driver.find_element_by_xpath("//*[contains(text(),'Хиты продаж')]/../../..")
-    for hit in sale_hits.find_elements_by_class_name('gallery-list-item'):
-        data_to_mongo['title'] = hit.find_element_by_css_selector('.fl-product-tile-title__link.sel-product-tile-title').get_attribute('title')
-        data_to_mongo['price_hits'] = hit.find_element_by_class_name('fl-product-tile-price__current').text
-        #pprint(data_to_mongo)
-        list_of_data.append(data_to_mongo)
+        sale_hits=driver.find_element_by_xpath("//*[contains(text(),'Хиты продаж')]/../../..")
+        for hit in sale_hits.find_elements_by_class_name('gallery-list-item')[:5]:
+            data_to_mongo = {}
+            data_to_mongo['title'] = hit.find_element_by_css_selector('.fl-product-tile-title__link.sel-product-tile-title').get_attribute('title')
+            data_to_mongo['price_hits'] = hit.find_element_by_class_name('fl-product-tile-price__current').text
+            if len(list_of_data)>0:
+                if data_to_mongo in list_of_data:
+                    continue
+                else:
+                    list_of_data.append(data_to_mongo)
+
+            else:
+                list_of_data.append(data_to_mongo)
+
+
+
     pprint(list_of_data)
     #coll.insert_many(list_of_data)
 except ElementClickInterceptedException:
